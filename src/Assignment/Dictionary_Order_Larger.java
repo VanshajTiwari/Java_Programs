b0cbe949642732dee36e6218b51da0817d1e92ad
VanshajTiwari <vanshajtiwari62@gmail.com>
2023-02-03 00:57:31 +0530

    modified:   src/Assignment/Dictionary_Order_Larger.java

--- src/Assignment/Dictionary_Order_Larger.java
+++ src/Assignment/Dictionary_Order_Larger.java
@@ -1,42 +1,24 @@
-package Assignment;
-
 import java.util.*;
-import java.util.Collections;
-
 public class Dictionary_Order_Larger {
-    public static void main(String[] args) {
-        Scanner input =new Scanner(System.in);
-        String abc=input.next();
-
-
-       List<String> result=permutationsList("",abc);
-       Collections.sort(result);
-       System.out.println(result);
-       for(int i=0;i<result.size();i++){
-           if(result.get(i).equals(abc)){
-               if(i+1<result.size()-1)
-                System.out.println(result.get(i+1));
-           }
-       }
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        List<String>ll=new ArrayList<>();
+        lexico(str,"",str,ll);
+        Collections.sort(ll);
+        for(int i=0;i<ll.size();i++)
+            System.out.println(ll.get(i));
     }
-    static List<String> permutationsList(String p, String up) {
-        if (up.isEmpty()) {
-            List<String> list = new ArrayList<>();
-            list.add(p);
-            return list;
+    public static void lexico(String a ,String b,String c,List<String>ll){
+        if(a.length()==0 && c.compareTo(b)<0){
+            ll.add(b);
+            return;
         }
-        char ch = up.charAt(0);
-
-        ArrayList<String> ans = new ArrayList<>();
-
-        for (int i = 0; i <= p.length(); i++) {
-            String f = p.substring(0, i);
-            String s = p.substring(i, p.length());
-            ans.addAll(permutationsList(f + ch + s, up.substring(1)));
+        for(int i=0;i<a.length();i++){
+            String s1=a.substring(0,i);
+            String s2=a.substring(i+1);
+            char ch=a.charAt(i);
+            lexico(s1+s2,b+ch,c,ll);
         }
-        return ans;
     }
-
-
-
 }
